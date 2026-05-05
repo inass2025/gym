68f02864a3337934861005b614cd3de4b04fa99a
Hiba <laqbourihiba@gmail.com>
2026-05-05 15:38:18 +0100

    local changes

--- config/cors.php
+++ config/cors.php
@@ -18,7 +18,7 @@ return [
     'paths' => ['api/*', 'sanctum/csrf-cookie'],
 
     'allowed_methods' => ['*'],
-    'allowed_origins' => ['http://192.168.51.138'],
+    'allowed_origins' => ['http://localhost:3000'],
 
     
 
